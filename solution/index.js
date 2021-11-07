f864367791f5a74660003f33cc6036303bdc66a3
zlobnikov <daffodil@list.ru>
2021-11-07 12:43:33 +0300

    feat: implement basic methods

--- solution/index.js
+++ solution/index.js
@@ -1,3 +1,48 @@
 module.exports = class {
-  // реализация
-}
+  constructor(array = []) {
+    this.array = [];
+    for (const value of array) this.add(value);
+  }
+
+  [Symbol.iterator]() {
+    return this.array[Symbol.iterator]();
+  }
+
+  get [Symbol.toStringTag]() {
+    return '^_^';
+  }
+
+  get size() {
+    return this.array.length;
+  }
+
+  add(value) {
+    if (!this.has(value)) this.array.push(value);
+    return this;
+  }
+
+  has(value) {
+    return this.array.includes(value);
+  }
+
+  delete(value) {
+    const index = this.array.indexOf(value);
+    this.array.splice(index, 1);
+  }
+
+  clear() {
+    this.array.length = 0;
+  }
+
+  keys() {
+    return this.array;
+  }
+
+  values() {
+    return this.array;
+  }
+
+  entries() {
+    return this.array.map(value => [value, value]);
+  }
+};
